dc11f6238df634370d78f09876f33bf45c2e096d
Sebastian Erives <justanormal41@gmail.com>
2021-01-26 05:08:46 +0000

    Comando %modmail y respuesta a “ayuda”

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import discord
 import os
 import sys, traceback
 import datetime
-from re import match
+import re
 
 bot = discord.Client()
 
@@ -16,7 +16,7 @@ def start(discord_token):
 async def on_ready():
     print('Logged in as {0}!'.format(bot.user))
     
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Rocket League"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Fornait"))
 
 @bot.event
 async def on_message(message):
@@ -30,6 +30,11 @@ async def on_message(message):
     await cmd_team(message)
   elif is_command(message.content, "participants"):
     await cmd_participants(message)
+  elif is_command(message.content, "modmail"):
+    await cmd_modmail(message)
+  
+  if re.search('ayuda', message.content, re.IGNORECASE) and not message.author.bot:
+    await message.channel.send("<@!{0}>, si necesitas ayuda de un staff usa el comando %modmail".format(message.author.id))
 
 async def cmd_set_team(message):
   if is_staff(message.author):
@@ -37,13 +42,10 @@ async def cmd_set_team(message):
       args = message.content.replace(prefix + "setteam ", "").split(" ")
 
       if len(args) < 2 or len(message.mentions) < 1:
-        
         await message.channel.send("Argumentos invalidos (A), usa ```{0}setteam <usuario(s)> <numero de equipo>```".format(prefix))
-        
         return
 
       try:
-
         team_number = int(args[len(message.mentions)].strip())
         role_name = "Team " + str(team_number)
         role = discord.utils.get(message.guild.roles, name=role_name)
@@ -57,7 +59,6 @@ async def cmd_set_team(message):
           await message.channel.send("Agregado el rol {0} a los usuarios especificados".format(role_name))
 
         return
-
       except:
         await message.channel.send("Argumentos invalidos (B), usa ```{0}setteam <usuario(s)> <numero de equipo>```".format(prefix))
   else:
@@ -71,9 +72,7 @@ async def cmd_team(message):
     return
 
   try:
-
     team_number = int(args[0].strip())
-
     role_name = "Team " + str(team_number)
     
     role = discord.utils.get(message.guild.roles, name=role_name)
@@ -91,7 +90,7 @@ async def cmd_team(message):
     members_str = ""
 
     for member in members:
-        members_str += "\n" + member.name
+        members_str += "\n" + get_name_or_nick(member)
             
     if(members_str.strip() == ""):
       await message.channel.send("El equipo especificado no tiene ningun miembro")
@@ -100,7 +99,6 @@ async def cmd_team(message):
     await message.channel.send("Miembros en el " + role_name + " ```" + members_str + "```")
 
     return
-
   except:
 
     await message.channel.send("Argumentos invalidos (B) , usa ```{0}team <numero de equipo>```".format(prefix))
@@ -120,7 +118,7 @@ async def cmd_participants(message):
 
   for member in members:
     members_count += 1
-    members_str += "\n" + member.name
+    members_str += "\n" + get_name_or_nick(member)
 
   await message.channel.send("```" + members_str + "```")
   await message.channel.send("Cantidad de participantes: {0}".format(members_count))
@@ -136,11 +134,28 @@ async def cmd_teams(message):
     final_msg += "\n" + role.name + ":"
     members = await members_in_role(message.guild, role.name)
     for member in members:
-      final_msg += "\n  " + member.name
+      final_msg += "\n  " + get_name_or_nick(member)
 
   final_msg += "```"
 
-  await await wait_message.edit(final_msg)
+  await wait_message.edit(content=final_msg)
+
+async def cmd_modmail(message):
+  msg = message.content.replace(prefix + "modmail", "").strip()
+
+  if msg == "":
+    await message.channel.send("Argumentos invalidos, usa ```%modmail <mensaje>```")
+    return
+
+  modmail_channel = discord.utils.get(message.guild.channels, name="modmail", type=discord.ChannelType.text)
+
+  if modmail_channel == None:
+    await message.channel.send("No se ha encontrado el canal #modmail!\nStaff: Asegurense de crear el canal antes mencionado (exactamente como esta escrito).")
+    return
+
+  await modmail_channel.send("@here, modmail de <@!{0}>: ```{1}```\nPresiona a este link para ir al mensaje: {2}".format(message.author.id, msg, message.jump_url))
+
+  await message.channel.send("Mensaje enviado, espera la respuesta de algun miembro del staff pronto.")
 
 def is_command(content, command):
   return content.startswith(prefix + command)
@@ -177,5 +192,13 @@ async def members_in_role(guild, role_name):
         members_in_role.append(member)
   return members_in_role
 
+def get_name_or_nick(member):
+  name = ""
+  if member.nick == None:
+    name = member.name
+  else:
+    name = member.nick 
+  return name
+
 def contains_mention(content, mention_id):
-  return match("<@!{0}>".format(mention_id), content) is not None
+  return re.match("<@!{0}>".format(mention_id), content) is not None
